e1b15631846331726689fe35c6eeb61de8e4b5d3
Anmol Gupta <iitkanmol438@gmail.com>
2024-06-16 13:58:13 +0530

    version update

--- env_package/snake_mania/setup.py
+++ env_package/snake_mania/setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 
 setup(name = 'snakemania',
-    version = '0.0.1',
-    install_requires = ['gym', 'pygame'])
+    version = '0.1.0',
+    install_requires = ['gym>=0.22.0, <=0.23.0', 'pygame>=2.1.0'])
